8ea693855eb93c47dbbae2ea689bfd0f1eac7c06
Neelabh <neelabhsomani1818@gmail.com>
2022-10-25 18:37:46 +0530

    Final commit

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import os
-import time as t
 import sqlite3 as sq 
 from matplotlib import pyplot as plt 
 from expendo import Expendo
-import sys
-import traceback
 
 def main():
     expendo_menu = Expendo()
@@ -12,25 +9,18 @@ def main():
     menu_choice = expendo_menu.user_choice()
     while menu_choice != 8:
         if menu_choice == 1:
-            t.sleep(2)
             expendo_menu.load_db()
         elif menu_choice==2:
-            t.sleep(2)
             expendo_menu.exp_statement()
         elif menu_choice==3:
-            t.sleep(2)
             expendo_menu.add_expense()
         elif menu_choice==4:
-            t.sleep(2)
             expendo_menu.month_bank()
         elif menu_choice==5:
-            t.sleep(2)
             expendo_menu.delete_entry()
         elif menu_choice==6:
-            t.sleep(2)
             expendo_menu.update_entry()
         elif menu_choice==7:
-            t.sleep(2)
             expendo_menu.visuals()
         menu_choice = expendo_menu.user_choice()
         
