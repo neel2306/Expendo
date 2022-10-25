29b6a8645dcb545591573efc44e68f9b0dfeed97
Neelabh <neelabhsomani1818@gmail.com>
2022-10-25 18:13:11 +0530

    Added the time  sleep function. Also updated the menu to show the database connection option first as the main menu and then followed by the operations menu.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time as t
 import sqlite3 as sq 
 from matplotlib import pyplot as plt 
 from expendo import Expendo
@@ -7,21 +8,29 @@ import traceback
 
 def main():
     expendo_menu = Expendo()
+    expendo_menu.load_db()
     menu_choice = expendo_menu.user_choice()
     while menu_choice != 8:
         if menu_choice == 1:
+            t.sleep(2)
             expendo_menu.load_db()
         elif menu_choice==2:
+            t.sleep(2)
             expendo_menu.exp_statement()
         elif menu_choice==3:
+            t.sleep(2)
             expendo_menu.add_expense()
         elif menu_choice==4:
+            t.sleep(2)
             expendo_menu.month_bank()
         elif menu_choice==5:
+            t.sleep(2)
             expendo_menu.delete_entry()
         elif menu_choice==6:
+            t.sleep(2)
             expendo_menu.update_entry()
         elif menu_choice==7:
+            t.sleep(2)
             expendo_menu.visuals()
         menu_choice = expendo_menu.user_choice()
         
